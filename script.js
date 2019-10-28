b09aad0e7fce65fff58d69cd1080e8ab586b6142
Moritz Göckel <moritz.goeckel@gmail.com>
2019-10-28 16:23:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function checkUrl(){
         $("#meta").remove(); // Description
         $("#menu-container").remove(); // Not sure :)
         $("ytd-video-primary-info-renderer").css({ 'border-bottom' : '0', 'padding': '8px 0 8px 0'}); // Border before comments
-        $("#columns.ytd-watch-flexy").css({'justify-content': 'left', '-webkit-justify-content': 'left'}); // Let title go all the way to the left
-
+        $("#columns").css({'justify-content': 'left', '-webkit-justify-content': 'left', 'max-width':'100%'}); // Let title go all the way to the left
+        
         // Optional: Date and views
         $("#info-text").remove()
         $(".super-title").remove(); // Hashtags
